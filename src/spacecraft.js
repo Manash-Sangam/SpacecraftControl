ab98c4822d63206afb346b2448f001616631f694
Manash Sangam <manashsangam04@gmail.com>
2023-08-16 12:07:46 +0530

    Spacecraft successfully moves up and down

--- src/spacecraft.js
+++ src/spacecraft.js
@@ -1,9 +1,44 @@
 class Spacecraft {
-    constructor(x, y, z, direction) {//Spacecraft Structure
+    //Spacecraft Structur
+    constructor(x, y, z, direction) {
         this.x = x;
         this.y = y;
         this.z = z;
         this.direction = direction;
+    }
+
+    //Move Forward
+    moveForward(){
+        if (this.direction === 'N') {
+            this.y += 1;
+        } else if (this.direction === 'S') {
+            this.y -= 1;
+        } else if (this.direction === 'E') {
+            this.x += 1;
+        } else if (this.direction === 'W') {
+            this.x -= 1;
+        } else if (this.direction === 'Up') {
+            this.z += 1;
+        } else if (this.direction === 'Down') {
+            this.z -= 1;
+        }
+    }
+    
+    //Move Backward
+    moveBackward() {
+        if (this.direction === 'N') {
+            this.y -= 1;
+        } else if (this.direction === 'S') {
+            this.y += 1;
+        } else if (this.direction === 'E') {
+            this.x -= 1;
+        } else if (this.direction === 'W') {
+            this.x += 1;
+        } else if (this.direction === 'Up') {
+            this.z -= 1;
+        } else if (this.direction === 'Down') {
+            this.z += 1;
+        }
     }
 }
 
--- test/spacecraft.test.js
+++ test/spacecraft.test.js
@@ -2,6 +2,7 @@ const { expect } = require('chai');
 const Spacecraft = require('../src/spacecraft');
 
 describe('Spacecraft Class', () => {
+    //Testing if the spacecraft builds correctly of not
     it('should initialize with correct position and direction', () => {
         const spacecraft = new Spacecraft(0, 0, 0, 'N');
         expect(spacecraft.x).to.equal(0);
@@ -10,5 +11,19 @@ describe('Spacecraft Class', () => {
         expect(spacecraft.direction).to.equal('N');
     });
 
+    //Testing if the spacecraft moves forward correctly or not
+    it('should move spacecraft forward correctly', () => {
+        const spacecraft = new Spacecraft(0, 0, 0, 'N');
+        spacecraft.moveForward();
+        expect(spacecraft.y).to.equal(1);
+    });
+
+
+    //Testing if the spacecraft moves backwared correctly or not
+    it('should move spacecraft backward correctly', () => {
+        const spacecraft = new Spacecraft(0, 0, 0, 'N');
+        spacecraft.moveBackward();
+        expect(spacecraft.y).to.equal(-1);
+    });
 });
 
