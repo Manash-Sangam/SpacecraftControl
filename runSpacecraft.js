38e60c210b87b5b4ff77c9b7092815d38bb75e27
Manash Sangam <manashsangam04@gmail.com>
2023-08-16 13:39:22 +0530

    Sample Runner File Created

---
+++ runSpacecraft.js
@@ -0,0 +1,17 @@
+//Import
+const Spacecraft = require('./src/spacecraft');
+const executeCommands = require('./src/executecommands');
+
+//New Spacecraft
+const spacecraft = new Spacecraft(0, 0, 0, 'N');
+
+//List of Commands
+executecommandarray=['f', 'r', 'u', 'b', 'l']
+
+//Store result after executing commands
+const result = executeCommands(spacecraft, executecommandarray);
+
+//Result
+console.log("After executing commands: ",executecommandarray);
+console.log("Spacecraft is at position: ",result.finalPosition);
+console.log("Spacecraft is facing direction: ",result.finalDirection);
